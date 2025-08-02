106374720387e7583f306d34b08b87de795d018a
ronqstrike <ronmaru009@gmail.com>
2025-08-02 10:55:22 +0800

    updated animate on scroll behavior to animate when scrolled up

--- assets/js/main.js
+++ assets/js/main.js
@@ -241,9 +241,9 @@
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
+      easing: 'ease-in-out-back',
+      once: false,
+      mirror: true
     })
   });
 
